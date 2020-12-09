60107803dc3057ab7497e4c2c10e0ea3effd0e1e
J. Luis López G <lgjoseluis@hotmail.com>
2020-12-08 21:38:56 -0600

    Update project namespaces

--- ApplicationMobile/UIScheduler/UIScheduler.UWP/MainPage.xaml.cs
+++ ApplicationMobile/UIScheduler/UIScheduler.UWP/MainPage.xaml.cs
@@ -13,7 +13,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-namespace UIScheduler.UWP
+namespace WIScheduler.UIScheduler.UWP
 {
     public sealed partial class MainPage
     {
